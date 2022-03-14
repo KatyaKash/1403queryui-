ae0e6fd4a563bde4a1ac9e3de54d6a9d42fd8b82
katya&Lilya <yurihiyakana15@gmail.com>
2022-03-14 13:04:55 +0700

    3.1

--- s.js
+++ s.js
@@ -1,24 +1,3 @@
 $(document).ready(function(){
-	$("#el1").draggable();
-	$("#el1").resizable();
-
-	$("#sort1").sortable();
-
-	$("#el2").draggable({stack:"#drop1, drop2", revert:true});
-	$("#el3").draggable({stack:"#drop1, #drop2"});
-	$("#drop1").droppable({
-		accept: "#el2, #el3",
-		drop:function(event, ui){
-			ui.draggable.css("display", "none");
-			ui.draggable.css("display", "none");
-			$("#drop1").css("background-color", "green");
-		}
-	});
-	$("#drop2").droppable({
-		accept:"#el3",
-		drop:function(){
-			$("#el3").css("display", "none");
-			$("#drop2").css("background-color", "green");
-		}
-	});
+	$("#accordion1").accordion();
 });
